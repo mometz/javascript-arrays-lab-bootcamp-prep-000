8bb4d310ee2248c02bb9cf27ad6fefdac74aab86
Moritz M <moritz.muehlhausen@gmail.com>
2018-09-03 20:16:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Add your functions and code here
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name) {
+  return kittens.concat(name)
+}
+
+function prependKitten(name) {
+  var kittens2 = [name] 
+  return kittens2.concat(kittens)
+  }
+  
+function removeLastKitten() {
+  var kittens2 = kittens.splice(-1, 1)
+  return kittens2  
+  }
+  
